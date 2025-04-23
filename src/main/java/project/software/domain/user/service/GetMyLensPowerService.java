8bb8587d95590a7cf6537bbaf960e154f702ca7e
tae.y <0211ilyoil@gmail.com>
2025-04-23 22:06:43 +0900

    chore :: ddl 수정

--- src/main/java/project/software/domain/user/service/GetMyLensPowerService.java
+++ src/main/java/project/software/domain/user/service/GetMyLensPowerService.java
@@ -1,6 +1,5 @@
 package project.software.domain.user.service;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +11,7 @@ import project.software.domain.user.domain.User;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class GetMyLensPowerService {
+
     private final UserFacade userFacade;
 
     public GetMyLensPowerResponse execute() {
