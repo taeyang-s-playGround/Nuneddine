46b07ed00fbda89ca4abc622cb8a37facc00b692
tae.y <0211ilyoil@gmail.com>
2025-04-30 23:00:04 +0900

    feat :: 장바구니 추가 API

--- src/main/java/project/software/domain/cart/controller/CartController.java
+++ src/main/java/project/software/domain/cart/controller/CartController.java
@@ -2,25 +2,36 @@ package project.software.domain.cart.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import project.software.domain.cart.controller.dto.response.AllCartResponse;
 import project.software.domain.cart.domain.Cart;
 import project.software.domain.cart.domain.repository.CartRepository;
+import project.software.domain.cart.service.AddCartService;
 import project.software.domain.cart.service.GetAllCartService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/carts")
+@Validated
 public class CartController {
 
+    private final AddCartService addCartService;
     private final GetAllCartService getAllCartService;
 
     @GetMapping
     public AllCartResponse getAllCart() {
         return getAllCartService.execute();
+    }
+
+    @GetMapping("/{shop-id}")
+    public void addCart(@PathVariable ("shop-id") Long shopId, @RequestParam(value = "count", defaultValue = "1L") Long count) {
+        addCartService.execute(shopId, count);
     }
 }
--- src/main/java/project/software/domain/cart/service/AddCartService.java
+++ src/main/java/project/software/domain/cart/service/AddCartService.java
@@ -3,13 +3,34 @@ package project.software.domain.cart.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import project.software.domain.auth.facade.UserFacade;
+import project.software.domain.cart.domain.Cart;
 import project.software.domain.cart.domain.repository.CartRepository;
+import project.software.domain.shop.domain.Shop;
+import project.software.domain.shop.domain.repository.ShopRepository;
+import project.software.domain.shop.exception.ShopNotFoundException;
+import project.software.domain.user.domain.User;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class AddCartService {
 
+    private final UserFacade userFacade;
     private final CartRepository cartRepository;
+    private final ShopRepository shopRepository;
 
+    public void execute(Long shopId, Long count) {
+
+        User user = userFacade.GetCurrentUser();
+        Shop shop = shopRepository.findById(shopId)
+            .orElseThrow(() -> ShopNotFoundException.EXCEPTION);
+
+        cartRepository.save(Cart.builder()
+                .count(count)
+                .user(user)
+                .shop(shop)
+            .build());
+
+    }
 }
