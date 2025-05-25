2d829f6eb28303cf45bfda0027eb92108f20fa3c
tae.y <0211ilyoil@gmail.com>
2025-05-25 20:26:31 +0900

    feat :: delete cart API

---
+++ src/main/java/project/software/domain/cart/service/DeleteCartService.java
@@ -0,0 +1,33 @@
+package project.software.domain.cart.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.software.domain.auth.facade.UserFacade;
+import project.software.domain.cart.domain.Cart;
+import project.software.domain.cart.domain.repository.CartRepository;
+import project.software.domain.cart.exception.CartNotFoundException;
+import project.software.domain.user.domain.User;
+import project.software.domain.user.exception.UserMisMatchException;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class DeleteCartService {
+
+    private final CartRepository cartRepository;
+    private final UserFacade userFacade;
+
+    public void execute(Long cartId) {
+
+        User user = userFacade.getCurrentUser();
+
+        Cart cart = cartRepository.findById(cartId).orElseThrow(()-> CartNotFoundException.EXCEPTION);
+
+        if (!cart.getUser().getId().equals(user.getId())) {
+            throw UserMisMatchException.EXCEPTION;
+        }
+
+        cartRepository.delete(cart);
+    }
+}
