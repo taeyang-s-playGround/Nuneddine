5c996ec11b4f76b80b402054f30557e6c06fc93f
tae.y <0211ilyoil@gmail.com>
2025-05-26 16:14:52 +0900

    fix :: request Notblank

--- src/main/java/project/software/domain/cart/controller/dto/request/ChangeOptionRequest.java
+++ src/main/java/project/software/domain/cart/controller/dto/request/ChangeOptionRequest.java
@@ -3,12 +3,17 @@ package project.software.domain.cart.controller.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
 @NoArgsConstructor
 @Getter
 public class ChangeOptionRequest {
 
+    @NotBlank
     private Float lensPower;
 
+    @NotBlank
     private Long count;
 
 }
