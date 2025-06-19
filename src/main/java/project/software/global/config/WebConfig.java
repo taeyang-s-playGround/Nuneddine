07662ec62cce7e5aa9ea50fbf6d37d16aafc7abf
tae.y <0211ilyoil@gmail.com>
2025-06-19 10:48:50 +0900

    feat :: CORS 허용

--- src/main/java/project/software/global/config/WebConfig.java
+++ src/main/java/project/software/global/config/WebConfig.java
@@ -7,19 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
-                        .allowedHeaders("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
-                        .exposedHeaders("Authorization", "RefreshToken")
-                        .allowCredentials(false);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedOriginPatterns("*")
+            .allowedHeaders("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
+            .exposedHeaders("Authorization", "RefreshToken", "*")
+            .allowCredentials(false);
     }
 }
